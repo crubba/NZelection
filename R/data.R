e9a2f8106776e4286d79c17e707c0b74d7be5184
christian <christian.rubba@gmail.com>
2015-12-09 13:32:27 +0100

    lazy

--- R/data.R
+++ R/data.R
@@ -1,23 +1,22 @@
-#' Electoral results from New Zealand federal elections, 1996-2014.
+#' Votes.
 #'
-#' Complete data on German Bundestag elections form 1949 to 2013
+#' Complete data on New Zealand federal elections from 1996 to 2014.
 #'
 #' @format A data frame with seve variables: 
 #' \describe{
-#' \item{\code{year}}{year of the election}
-#' \item{\code{bundesland}}{the Bundesland of the \code{district}}
-#' \item{\code{district}}{the district name}
-#' \item{\code{wk}}{the district number}
-#' \item{\code{mandate}}{type of mandate, cvote = Candidate Vote, pvote = Party Vote (only for elections from 1953)}
-#' \item{\code{per}}{the percentage of votes for the \code{party}}
+#' \item{\code{year}}{the election year}
+#' \item{\code{district}}{the electoral district}
 #' \item{\code{party}}{the party name}
+#' \item{\code{mandate}}{the type of mandate for which the votes have been recorded (cvote = Candidate Vote, pvote = Party Vote)}
+#' \item{\code{votes}}{the number of votes}
+#' \item{\code{per}}{the percentage of votes for the \code{party}}
 #' }
+#' @source \url{http://www.electionresults.govt.nz/}
 "nz_votes"
 
-#' Information on candidates in New Zealand federal elections, 1995-2014.
+#' Candidate information.
 #'
-#' Information on candidates in Bundestag elections. Currently, information are
-#' only available from 1980 onward.
+#' Information on candidates in New Zealand federal elections.
 #'
 #' @format A data frame with 18 variables:
 #' #' \describe{
@@ -32,13 +31,14 @@
 #' \item{\code{gender}}{candidate's gender}
 #' \item{\code{dr}}{does candidate hold a PhD?}
 #' \item{\code{adel}}{does candidate have a noble name?}
-#' \item{\code{german}}{does candidate have a german name?}
+#' \item{\code{maori}}{does candidate have a german name?}
 #' \item{\code{on_list}}{is candidate' name on the party list?}
 #' \item{\code{on_wk}}{does the candidate run in a district?}
 #' \item{\code{toplevel}}{did candidate obtain high-level political office?}
 #' \item{\code{mdl}}{was candidate member of a state parliament before?}
 #' \item{\code{mdb}}{was candidate member of the Bundestag before?}
 #' }
+#' @source Self compiled from various sources
 "nz_candidates"
 
 #' Joint distribution of candidate and party vote in New Zealand federal elections, 1996-2014
@@ -47,31 +47,31 @@
 #'
 #' @format A data frame with nine variables:
 #' \describe{
-#' \item{\code{x}}{age in years}
-#' \item{\code{qx}}{probability of death at age \code{x}}
-#' \item{\code{lx}}{number of survivors, of birth cohort of 100,000, at next integral age}
-#' \item{\code{dx}}{number of deaths that would occur between integral ages}
-#' \item{\code{Lx}}{Number of person-years lived between \code{x} and \code{x+1}}
-#' \item{\code{Tx}}{Total number of person-years lived beyond age \code{x}}
-#' \item{\code{ex}}{Average number of years of life remaining for members of cohort alive at age \code{x}}
-#' \item{\code{sex}}{Sex}
-#' \item{\code{year}}{Year}
+#' \item{\code{year}}{election year}
+#' \item{\code{district}}{the electoral district}
+#' \item{\code{cvote}}{the party of the candidate vote}
+#' \item{\code{pvote}}{the party of the party vote} 
+#' \item{\code{votes}}{the number of votes}
+#' \item{\code{per}}{percentage of votes}
 #' }
+#' @source \url{http://www.electionresults.govt.nz/}
 #'
-#' For further details, see \url{http://www.ssa.gov/oact/NOTES/as120/LifeTables_Body.html#wp1168594}
+#' For further details, see \url{http://www.electionresults.govt.nz/}
 #'
 "nz_splitvotes"
 
-#' Polling place information
+#' Polling place information.
 #'
-#' Closeness of Bundestag districts
+#' Electoral outcomes at the polling station level
 #'
 #' @format A data frame:
 #' \describe{
-#' \item{\code{id}}{number for district 1}
-#' \item{\code{id_from}}{number for district 2}
-#' \item{\code{dis}}{distance between \code{id} and \code{id_from}}
-#' \item{\code{year}}{year of the election}
-#' \item{\code{wp}}{the legislative period}
+#' \item{\code{year}}{the election year}
+#' \item{\code{district}}{the electoral district}
+#' \item{\code{place}}{the polling place}
+#' \item{\code{party}}{the party name}
+#' \item{\code{mandate}}{the mandate type for which the votes have been recorded}
+#' \item{\code{votes}}{the number of votes for the party at the polling place}
 #' }
+#' @source \url{http://www.electionresults.govt.nz/}
 "nz_pollingplaces"
--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 # About
-This R package contains data sets on New Zealand parliamentary elections, from 1996 to 2014. It comprises three seperate data sets: 
+This R package contains data sets on New Zealand parliamentary elections (under MMP), from 1996 to 2014. It comprises three seperate data sets: 
 
-- Electoral results
+* Electoral results
   1. District level, party and candidate votes (*nz_votes*)
   2. District level, joint distribution of party and candidate votes (*nz_splitvotes*)
   2. Polling place, party and candidate votes (*nz_pollingplaces*)
-- Candidate information -- *nz_candidates* -- 
+* Candidate information (*nz_candidates*)
 
 The information was compiled from open information. The associated scripts have   [https://github.com/crubba/Project-NZelection](on this Github repo). Comma-separated files can be downloaded from [here](http://www.christianrubba.com/projects/projects.html#NZelection). 
 
 # Installation & Usage
 The package can be from this Github repository:
 
-```{r, eval = F}
-install.packages("devtools")
+
+```r
+#install.packages("devtools")
 devtools::install_github("crubba/NZelection")
 ```
 
 After installation, access to the data is possible by typing:
 
-```{r, eval = F}
+
+```r
 library(NZelection)
 
 data(nz_votes)
@@ -29,9 +31,7 @@ data(nz_candidates)
 ```
 
 # Example
-```{r, eval = F}
 
-```
 
 # Helper scripts
 Alongside the data, the package also provides convenience functions for producing data frames that are suitable for certain kinds of statistical analysis
@@ -43,7 +43,8 @@ Alongside the data, the package also provides convenience functions for producin
 # Citation
 If you use the datasets provided through this package, please consider citing it. Once installed, type
 
-```{r, eval = F}
+
+```r
 citation("NZelection")
 ```
 
