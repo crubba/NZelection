b30fb30926b49338a5d07910d30b6c66a7843d2c
Christian <christian.rubba@gmail.com>
2016-09-03 11:32:52 +0200

    v.0.6.0

--- tests/testthat/test_nze_splitvotes.R
+++ tests/testthat/test_nze_splitvotes.R
@@ -3,7 +3,12 @@ context("nze_splitvotes")
 test_that("Sensible ranges", {
 	
 	data("nze_splitvotes")
-	rangedata <- range(nze_splitvotes$per, na.rm = TRUE)
-
+	rangedata <- range(nze_splitvotes$per_joint, na.rm = TRUE)
   expect_that(all(rangedata >= 0 & rangedata <= 1), is_true())
+  
+  rangedata2 <- range(nze_splitvotes$per_bar_pvote, na.rm = TRUE)
+  expect_that(all(rangedata2 >= 0 & rangedata2 <= 1), is_true())
+  
+  rangedata3 <- range(nze_splitvotes$per_bar_cvote, na.rm = TRUE)
+  expect_that(all(rangedata3 >= 0 & rangedata3 <= 1), is_true())
 })
